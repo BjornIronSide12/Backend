087d98868756a33502e3b1ff99e2594182769502
Raghvendra <raghvendrasonarikar@gmail.com>
2024-09-22 19:54:47 +0530

    Created FriendMS, PostMS, RequestMS, DataMS

---
+++ DataMS/src/main/java/com/facebook/DataMS/DataMsApplication.java
@@ -0,0 +1,13 @@
+package com.facebook.DataMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMsApplication.class, args);
+	}
+
+}
